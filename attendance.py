ba39a430b928e994b68d9e1a69e30560a85708f7
crispim <crispim1411@gmail.com>
2021-06-20 19:42:00 -0300

    arquivo especifico attendance.py

--- attendance.py
+++ attendance.py
@@ -1,6 +1,32 @@
 import config
+import discord
 
-def process(message)
-    message = message[1:]
+async def process_data(message, attendance_list):
+    itens = message.content.split(' ')
 
-    if message.startswith('subscribe')
+    if message.content.startswith(config.PREFIX + 'eventos'):
+        await message.channel.send('Eventos cadastrados: ')
+        for event in attendance_list.keys():
+            await message.channel.send(event)
+        return
+
+    if len(itens) < 2:
+        await message.channel.send("Por favor, digite o nome do evento.")
+        return 
+        
+    content = ' '.join(itens[1:])
+
+    if message.content.startswith(config.PREFIX + 'novo'):
+        attendance_list[content] = []
+        msg = await message.channel.send(f'Interaja aqui para se inscrever na lista de alunos de **{content}**')
+        await msg.add_reaction(config.EMOJI)
+
+    elif message.content.startswith(config.PREFIX + 'chamada'):
+        if content not in attendance_list.keys():
+            await message.channel.send("Evento não cadastrado.")
+            return
+
+        if len(attendance_list[content]) == 0:
+            await message.channel.send("Não há alunos inscritos.")
+        for i, user in enumerate(attendance_list[content], start=1):
+            await message.channel.send(f'{i}) {user}')
--- bot.py
+++ bot.py
@@ -1,45 +1,13 @@
 import discord
 from discord.utils import get
 from discord.ext import commands
-import config
 import asyncio 
 
-BOT = 'Lista de chamada#1795' # trocar para #1795
-AUTHORS = ['Crispim#8453', 'crispim2#6967']
-EMOJI = '\U00002705'
+import config
+from attendance import process_data
 
 client = commands.Bot(command_prefix = config.PREFIX)
 attendance_list = {}
-
-async def process_data(message, attendance_list):
-    itens = message.content.split(' ')
-
-    if message.content.startswith(config.PREFIX + 'eventos'):
-        await message.channel.send('Eventos cadastrados: ')
-        for event in attendance_list.keys():
-            await message.channel.send(event)
-        return
-
-    if len(itens) < 2:
-        await message.channel.send("Por favor, digite o nome do evento.")
-        return 
-        
-    content = ' '.join(itens[1:])
-
-    if message.content.startswith(config.PREFIX + 'novo'):
-        attendance_list[content] = []
-        msg = await message.channel.send(f'Interaja aqui para se inscrever na lista de alunos de _{content}_')
-        await msg.add_reaction(EMOJI)
-
-    elif message.content.startswith(config.PREFIX + 'chamada'):
-        if content not in attendance_list.keys():
-            await message.channel.send("Evento não cadastrado.")
-            return
-
-        if len(attendance_list[content]) == 0:
-            await message.channel.send("Não há alunos inscritos.")
-        for i, user in enumerate(attendance_list[content], start=1):
-            await message.channel.send(f'{i}) {user}')
             
 @client.event
 async def on_ready():
@@ -59,14 +27,15 @@ async def on_reaction_add(reaction, user):
     global attendance_list
 
     channel = client.get_channel(reaction.message.channel.id)
-    itens = reaction.message.content.split('_')
+    itens = reaction.message.content.split('**')
     reaction = str(reaction).encode('unicode-escape')
 
     if len(itens) > 1:
         current_event = itens[1]
 
-        if str(user) != BOT:
-            if reaction == EMOJI.encode('unicode-escape'):
+        if config.BOT not in str(user):
+            if reaction == config.EMOJI.encode('unicode-escape'):
+                # inserção no banco
                 attendance_list[current_event].append(user.mention)
                 await channel.send(f"{user.mention} inscrito(a) em {current_event}")
             
