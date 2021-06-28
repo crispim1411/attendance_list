3286a789c385f1baed1580c4bd12d4738be5c8d3
crispim <crispim1411@gmail.com>
2021-06-28 15:20:45 -0300

    modelo de base de dados feita por conexão a cada chamada

--- database.py
+++ database.py
@@ -7,106 +7,148 @@ try:
 except:
     from config import DATABASE_URL
 
-url = urlparse(DATABASE_URL)
-connection = psycopg2.connect(
-    dbname=url.path[1:],
-    user=url.username,
-    password=url.password,
-    host=url.hostname,
-    port=url.port
-    )
+def connect():
+    try:
+        url = urlparse(DATABASE_URL)
+        connection = psycopg2.connect(
+            dbname=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+            )
+        return connection
+    except Exception as e:
+        print(f"Error while establishing database connection: {e}")
+    
+def connect_to_database(func):
+    def wrapper(*args, **kwargs):
+        connection = connect()
+        args += (connection,)
+        return func(*args, *kwargs)
+    return wrapper
 
-def insert_event(name):
+
+@connect_to_database
+def insert_event(name, connection=None):
     event = find_event(name)
     if event:
         return False
 
-    with connection, connection.cursor() as cursor:
-        cursor.execute("""
-            INSERT INTO events(name)
-            VALUES (%s) RETURNING id;
-            """, (name,))
-        event_id = cursor.fetchone()[0]
-        return event_id
-
-def insert_user(name, mention, event_name):
-    event = find_event(event_name) 
-    with connection, connection.cursor() as cursor:
-        cursor.execute(""" 
-            SELECT U.* FROM users U
-            INNER JOIN events E ON E.id = U.event_id
-            WHERE E.name LIKE %s AND U.mention = %s
-            """, (event_name, mention,))
-        if cursor.fetchone() == None:
+    try:
+        with connection, connection.cursor() as cursor:
             cursor.execute("""
-                INSERT INTO users(name, mention, event_id)
-                VALUES (%s, %s, %s) RETURNING id;
-                """, (name, mention, event[0],))
-        else:
-            return False
+                INSERT INTO events(name)
+                VALUES (%s) RETURNING id;
+                """, (name,))
+            return cursor.fetchone()[0] #event_id
+    finally:
+        connection.close()
 
-def find_all_events():
-    with connection, connection.cursor() as cursor:
-        cursor.execute("SELECT * FROM events")
-        events = cursor.fetchall()
-        return events
+@connect_to_database
+def insert_user(name, mention, event_name, connection=None):
+    event = find_event(event_name) 
+    try:
+        with connection, connection.cursor() as cursor:
+            cursor.execute(""" 
+                SELECT U.* FROM users U
+                INNER JOIN events E ON E.id = U.event_id
+                WHERE E.name LIKE %s AND U.mention = %s
+                """, (event_name, mention,))
 
-def find_event(name):
-    with connection, connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT * FROM events
-            WHERE name LIKE %s
-            """, (name,))
-        event = cursor.fetchone()
-        return event
+            if cursor.fetchone() == None:
+                cursor.execute("""
+                    INSERT INTO users(name, mention, event_id)
+                    VALUES (%s, %s, %s) RETURNING id;
+                    """, (name, mention, event[0],))
+            else:
+                return False
+    finally:
+        connection.close()
 
-def find_user(mention):
-    with connection, connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT * FROM users
-            WHERE mention = %s
-            """, (mention,))
-        event = cursor.fetchall()
-        return event
+@connect_to_database
+def find_all_events(connection=None):
+    try:
+        with connection, connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM events")
+            return cursor.fetchall()
+    finally:
+        connection.close()
 
-def find_event_users(event_name):
-    with connection, connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT U.* FROM users U
-            INNER JOIN events E ON E.id = U.event_id
-            WHERE E.name LIKE %s
-            """, (event_name,))
-        users = cursor.fetchall()
-        return users
+@connect_to_database
+def find_event(name, connection):
+    try:
+        with connection, connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT * FROM events
+                WHERE name LIKE %s
+                """, (name,))
+            return cursor.fetchone()
+    finally:
+        connection.close()
 
-def delete_event(event_name):
+@connect_to_database
+def find_user(mention, connection=None):
+    try:
+        with connection, connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT * FROM users
+                WHERE mention = %s
+                """, (mention,))
+            return cursor.fetchall()
+    finally:
+        connection.close()
+
+@connect_to_database
+def find_event_users(event_name, connection=None):
+    try:
+        with connection, connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT U.* FROM users U
+                INNER JOIN events E ON E.id = U.event_id
+                WHERE E.name LIKE %s
+                """, (event_name,))
+            return cursor.fetchall()
+    finally:
+        connection.close()
+
+@connect_to_database
+def delete_event(event_name, connection=None):
     event = find_event(event_name) 
     if not event:
         return False
 
-    with connection, connection.cursor() as cursor:
-        cursor.execute("""
-            DELETE FROM users
-            WHERE event_id = %s""", (event[0],))
-        cursor.execute("""
-            DELETE FROM events
-            WHERE id = %s""", (event[0],))
-
-def delete_user(user_mention, event_name):
-    event = find_event(event_name) 
-    if not event:
-        return False
-
-    with connection, connection.cursor() as cursor:
-        cursor.execute("""
-            SELECT U.* FROM users U
-            INNER JOIN events E ON E.id = U.event_id
-            WHERE E.id = %s AND U.mention = %s
-            """, (event[0], user_mention))
-        user = cursor.fetchone()
-        if user:
+    try:
+        with connection, connection.cursor() as cursor:
             cursor.execute("""
                 DELETE FROM users
-                WHERE users.mention = %s""", (user[2],))
-        else:
-            return False
+                WHERE event_id = %s""", (event[0],))
+            cursor.execute("""
+                DELETE FROM events
+                WHERE id = %s""", (event[0],))
+    finally:
+        connection.close()
+
+@connect_to_database
+def delete_user(user_mention, event_name, connection=None):
+    event = find_event(event_name) 
+    if not event:
+        return False
+
+    try:
+        with connection, connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT U.* FROM users U
+                INNER JOIN events E ON E.id = U.event_id
+                WHERE E.id = %s AND U.mention = %s
+                """, (event[0], user_mention))
+            user = cursor.fetchone()
+            if user:
+                cursor.execute("""
+                    DELETE FROM users
+                    WHERE users.mention = %s""", (user[2],))
+            else:
+                return False
+    finally:
+        connection.close()
+    
