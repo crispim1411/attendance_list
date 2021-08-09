eb6a849783d250b2e3566e04cf1f7e53d16895f9
Crispim <crispim1411@gmail.com>
2021-08-09 00:37:26 -0300

    removido código antigo

--- bot.py
+++ bot.py
@@ -29,30 +29,6 @@ async def on_message(message):
         await process_command.process_data(message)
 
 @client.event
-async def on_reaction_add(reaction, user):
-    BOT = client.user.discriminator
-    if reaction.message.author.discriminator != BOT \
-        or user.discriminator == BOT:
-        return
-
-    if len(reaction.message.embeds) == 1:
-        embed_description = reaction.message.embeds[0].description
-        itens = embed_description.split('**')
-    else:
-        return
-
-    message = reaction.message
-    str_reaction = str(reaction).encode('unicode-escape')
-    if len(itens) > 1:
-        current_event = itens[1]
-
-        if str_reaction == config['CROSS'].encode('unicode-escape'):
-            if message.embeds[0].title == "Excluir evento":
-                await send_message.remove_event_response(message, user.mention, current_event)
-            else:
-                await send_message.remove_subscription_reponse(message, user.name, user.mention, current_event)
-
-@client.event
 async def on_button_click(interaction):
     user = interaction.author
     mention = f'<@!{user.id}>'
